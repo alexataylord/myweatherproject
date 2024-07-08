24915aaa174f2497278fe12b67cd2532930cf3f3
Alexa <alexataylord@gmail.com>
2024-07-08 11:27:14 -0400

    injected more weather data

--- src/file.js
+++ src/file.js
@@ -5,14 +5,15 @@ function refreshWeather(response) {
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windSpeedElement = document.querySelector("#wind-speed");
-
- 
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
     
+ 
+
 
     
     cityElement.innerHTML = response.data.city;
-
-   
+    timeElement.innerHTML = formatDate(date);   
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
@@ -20,7 +21,30 @@ function refreshWeather(response) {
 
 }
 
- 
+
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["sunday",
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+     ];
+     let day = days[date.getDay()];
+
+     if (minutes < 10){
+        minutes = `0${minutes}`;
+     }
+
+     return `${day} ${hours}:${minutes}`;
+}
+
+
+
+
 function searchCity(city) { 
     let apiKey = "45378823524b7c076fde8t64aafb9ofd";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;

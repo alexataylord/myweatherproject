97e49a1db6a832693411022da9d49854fa63bbbf
Alexa <alexataylord@gmail.com>
2024-08-05 12:03:01 -0400

    creating display forecast
    creating a function in JS

--- src/file.js
+++ src/file.js
@@ -52,7 +52,34 @@ function refreshWeather(response) {
         searchCity(searchInput.value);
       }
 
+      function displayForecast() {
+             let forecast = document.querySelector("#forecast");
+
+             let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+
+             days.forEach(function(day) {
+                 forecast.innerHTML = `
+      <div class="weather-forecast-day">
+      <div class="weather-forecast-date">${day}</div>
+      <div class="weather-forecast-icon">🌧️</div>
+      <div class="weather-forecast-temps">
+        <div class="weather-forecast-temperature">
+            <strong>15°</strong>
+            </div>
+            <div class="weather-forecast-temperature">9°</div>
+            </div>
+            </div>
+      `;
+
+             });
+
+     
+      }
+
       let searchFormElement = document.querySelector("#search-form");
       searchFormElement.addEventListener("submit", handleSearchSubmit);
 
       searchCity("Gold Coast");
+      displayForecast();
+
+ 
